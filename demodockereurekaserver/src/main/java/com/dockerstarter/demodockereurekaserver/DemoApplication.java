2eaf0c56618ae32439c5a95d2bc040b870fa1d05
Abhilash Patel email = patel.lipun@gmail.com <55509720+patelabhilash@users.noreply.github.com>
2020-05-19 17:11:31 +0530

    update import statement for enableeurekaserver

--- demodockereurekaserver/src/main/java/com/dockerstarter/demodockereurekaserver/DemoApplication.java
+++ demodockereurekaserver/src/main/java/com/dockerstarter/demodockereurekaserver/DemoApplication.java
@@ -2,6 +2,7 @@ package com.dockerstarter.demodockereurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
 @EnableEurekaServer
