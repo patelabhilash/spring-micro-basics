84ed69fce52dc819455c441c5e5d066985306398
Abhilash Patel email = patel.lipun@gmail.com <55509720+patelabhilash@users.noreply.github.com>
2020-05-19 16:26:34 +0530

    config server init commit

---
+++ demodock/src/main/java/com/dockerstarter/demodock/ConfigServer.java
@@ -0,0 +1,15 @@
+package com.dockerstarter.demodock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServer.class, args);
+	}
+
+}
