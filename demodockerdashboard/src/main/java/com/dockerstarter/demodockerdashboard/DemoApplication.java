4ac74972e3ba223357542419ac6bccfc370cafa4
Abhilash Patel email = patel.lipun@gmail.com <55509720+patelabhilash@users.noreply.github.com>
2020-05-23 11:57:50 +0530

    few updates

--- demodockerdashboard/src/main/java/com/dockerstarter/demodockerdashboard/DemoApplication.java
+++ demodockerdashboard/src/main/java/com/dockerstarter/demodockerdashboard/DemoApplication.java
@@ -3,7 +3,6 @@ package com.dockerstarter.demodockerdashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
